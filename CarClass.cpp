3443e8f03778854bef451a3783194e6dd1c34fc0
DeceptiKHAAAAN <127812076+DeceptiKHAAAAN@users.noreply.github.com>
2026-04-06 17:34:24 -0500

    Implemented Car Class

--- CarClass.cpp
+++ CarClass.cpp
@@ -1,1 +1,70 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+class Car
+{
+    public:
+        Car();
+        Car(int yearModel, string make);
+        int GetYearModel();
+        string GetMake();
+        int GetSpeed();
+        void SetYearModel(int newYearModel);
+        void SetMake(string newMake);
+        void SetSpeed(int newSpeed);
+        void Accelerate();
+        void Brake();
+    private:
+        int yearModel, speed;
+        string make;
+};
+
+int main()
+{
+    return 0;
+}
+
+Car::Car()
+{
+    yearModel = 0000;
+    make = "EMPTY";
+    speed = 0;
+}
+
+Car::Car(int yearModel, string make)
+{
+    this->yearModel = yearModel;
+    this->make = make;
+    speed = 0;
+}
+
+int Car::GetYearModel()
+{
+    return yearModel;
+}
+
+string Car::GetMake()
+{
+    return make;
+}
+
+int Car::GetSpeed()
+{
+    return speed;
+}
+
+void Car::SetYearModel(int newYearModel)
+{
+    yearModel = newYearModel;
+}
+
+void Car::SetMake(string newMake)
+{
+    make = newMake;
+}
+
+void Car::SetSpeed(int newSpeed)
+{
+    speed = newSpeed;
+}
